985b8cbf7e3035ea99406f7a50cf73278c10c937
Alexey <asdalexey@yandex.ru>
2018-07-18 23:00:14 +0300

    #7 template

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-basic',
@@ -6,4 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./basic.component.scss']
 })
 export class BasicComponent {
+  one = { message: 'Hello context' };
+  two = { message: 'Hello two' };
+  three = { message: 'Hello three' };
 }
